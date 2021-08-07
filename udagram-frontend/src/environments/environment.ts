a1029b95d1550ea90452fef90fb51ed6e361f14b
ruchi <ruchiverma6@gmail.com>
2021-08-07 22:59:04 +0530

    updated deployments file

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -1,11 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://afbad4fb3c6114aedab344a0198cb947-802599395.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
