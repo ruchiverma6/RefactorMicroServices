51df28a35e5c1067a677af26f9dbce9311ef2e74
ruchi <ruchiverma6@gmail.com>
2021-08-20 19:32:16 +0530

    Updated screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a4339d422c15c4da58502f5251b67050-583698251.us-west-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a355f3e77675a4628820d430aa4050c2-373756375.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
